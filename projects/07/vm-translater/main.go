b4098f8caf469b2b104d0a2151cc2e42fd772b44
Robert Curry <robert.curry@lexer.io>
2021-10-25 09:30:44 +1100

    io started

--- projects/07/vm-translater/main.go
+++ projects/07/vm-translater/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"log"
+	"os"
 	"vm-translater/codewriter"
 	"vm-translater/parser"
 )
@@ -10,4 +14,38 @@ func main(){
 	fmt.Println("hello world")
 	parser.Parse()
 	codewriter.Write()
+
+	filePath := "./test.txt"
+
+	data := readFile(filePath)
+	writeFile("./out.txt", data)
+
+	fmt.Println(data)
+}
+
+
+func readFile(filePath string) string {
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+	return string(data)
+}
+
+
+func writeFile(filePath, data string) {
+	message := []byte(data)
+	err := ioutil.WriteFile(filePath, message, fs.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
 }
